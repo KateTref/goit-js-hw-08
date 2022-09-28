8218517e949832c49df26a48c96b4a83f62f9796
KateTref <trefilovaekaterina408@gmail.com>
2022-09-29 01:19:44 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const picturesMarkup = createPicturesMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+function createPicturesMarkup(Items) {
+    return Items
+        .map(({ preview, original, description }) => {
+            return `
+       <li class="gallery__item>
+       <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+       </a>
+       </li>
+`}).join('');
+};
+
+ const lightbox = new SimpleLightbox('.gallery li', { captionsData: "alt", captionsDelay: 250 });
