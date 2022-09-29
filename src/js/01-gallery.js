20c1221c975ccec7ee2f1edb6f1e475b275322cb
KateTref <trefilovaekaterina408@gmail.com>
2022-09-29 14:14:25 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,6 @@
 import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryContainer = document.querySelector('.gallery');
 const picturesMarkup = createPicturesMarkup(galleryItems);
@@ -9,12 +11,19 @@ function createPicturesMarkup(Items) {
     return Items
         .map(({ preview, original, description }) => {
             return `
-       <li class="gallery__item>
+       <div class="gallery__item>
        <a class="gallery__link" href="${original}">
        <img class="gallery__image" src="${preview}" alt="${description}" />
        </a>
-       </li>
+       </div>
 `}).join('');
 };
 
- const lightbox = new SimpleLightbox('.gallery li', { captionsData: "alt", captionsDelay: 250 });
+const lightbox = new SimpleLightbox('.gallery div', {
+        captionType: 'attr',
+        animationSpeed: 250,
+        captionsData:'alt',
+        fadeSpeed: 250,
+        captionDelay: 250,
+    });
+
