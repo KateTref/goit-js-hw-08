9a79413c3480d1a381f80f0dcfad7b3e54f611b9
KateTref <trefilovaekaterina408@gmail.com>
2022-10-01 17:01:41 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from "lodash.throttle"; 
+import localStorage from "./storage";  
+
+
+const form = document.querySelector(".feedback-form");
+
+
+const FORM_STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", throttle(onFormInput, 500));
+form.addEventListener("submit", onFormSubmit);
+
+onPageLoad(form);
+
+function onPageLoad(formReference) {
+  const formDataObj = localStorage.load(FORM_STORAGE_KEY);
+  if (!formDataObj) {
+    return;
+  }
+  const {
+    elements: { email, message },
+  } = formReference;
+  email.value = formDataObj.email;
+  message.value = formDataObj.message;
+};
+
+function onFormInput(event) {
+  const formData = new FormData(event.target.closest('.feedback-form'));
+  const formDataObj = {};
+  formData.forEach((value, key) => {
+    formDataObj[key] = value;
+  });
+  localStorage.save(FORM_STORAGE_KEY, formDataObj);
+};
+
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const formDataObj = {};
+  formData.forEach((value, key) => {
+    formDataObj[key] = value;
+  });
+  if (!formDataObj.email) {
+    alert('Please fill email');
+    return;
+  } else if (!formDataObj.message) {
+    alert('Please fill message');
+    return;
+  } else {
+    console.log(formDataObj);
+    localStorage.remove(FORM_STORAGE_KEY);
+    event.currentTarget.reset();
+  }
+};
+
 
